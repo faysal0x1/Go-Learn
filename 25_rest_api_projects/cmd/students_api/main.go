526c9c99715d139e424181035082985c0ca27077
Faysal Rahman <faysal35-3119@diu.edu.bd>
2025-06-12 18:14:35 +0600

    feat: Implement Students API server with routing and error handling

--- 25_rest_api_projects/cmd/students_api/main.go
+++ 25_rest_api_projects/cmd/students_api/main.go
@@ -1,10 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/faysal0x1/Go-Learn/internal/config"
+)
 
 func main() {
+	// Initialize the configuration
 
-	fmt.Println("Starting Students API...")
+	cfg := config.MustLoad()
+
+	// Database connection setup
+
+	// Setup router
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Welcome to the Students API!"))
+	})
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Println("Starting Students API server on", cfg.Addr)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
+
+	fmt.Println("Students API is running on", cfg.Addr)
+	// Setup Server
+
 }
 
 // NewStudentsAPI initializes a new instance of the Students API.
--- 25_rest_api_projects/internal/config/config.go
+++ 25_rest_api_projects/internal/config/config.go
@@ -9,21 +9,12 @@ import (
 )
 
 type HTTPServer struct {
-	Addr string
+	Addr string `yaml:"address"`
 }
-
 type Config struct {
 	Env         string `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`
 	StoragePath string `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
 	HTTPServer  `yaml:"http_server" env:"HTTP_SERVER" env-required:"true"`
-	CORS        struct {
-		AllowOrigin      []string `yaml:"allow_origin" env:"ALLOW_ORIGIN" env-required:"true"`
-		AllowMethods     []string `yaml:"allow_methods" env:"ALLOW_METHODS" env-required:"true"`
-		AllowHeaders     []string `yaml:"allow_headers" env:"ALLOW_HEADERS" env-required:"true"`
-		ExposeHeaders    []string `yaml:"expose_headers" env:"EXPOSE_HEADERS"`
-		MaxAge           int      `yaml:"max_age" env:"MAX_AGE" env-required:"true"`
-		AllowCredentials bool     `yaml:"allow_credentials" env:"ALLOW_CREDENTIALS" env-required:"true"`
-	} `yaml:"cors" env:"CORS" env-required:"true"`
 }
 
 func MustLoad() *Config {

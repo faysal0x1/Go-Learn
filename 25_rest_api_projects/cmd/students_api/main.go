c7cd445275fca7a3e89525337ae2a163cd811eb4
Faysal Rahman <faysal35-3119@diu.edu.bd>
2025-06-12 18:45:52 +0600

    feat: Enhance Students API server with structured logging and graceful shutdown handling

--- 25_rest_api_projects/cmd/students_api/main.go
+++ 25_rest_api_projects/cmd/students_api/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/faysal0x1/Go-Learn/internal/config"
 )
@@ -29,14 +34,38 @@ func main() {
 		Handler: router,
 	}
 
-	fmt.Println("Starting Students API server on", cfg.Addr)
-	err := server.ListenAndServe()
+	slog.Info("Starting Students API server", cfg.Addr, slog.String("env", cfg.Env), slog.String("storage_path", cfg.StoragePath))
+
+	// fmt.Println("Starting Students API server on", cfg.Addr)
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	// Handle graceful shutdown
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatal("Error starting server: ", err)
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err := server.Shutdown(ctx)
 
 	if err != nil {
-		log.Fatal("Error starting server: ", err)
+		slog.Error("Error shutting down server: ", slog.String("error", err.Error()))
 	}
 
-	fmt.Println("Students API is running on", cfg.Addr)
+	slog.Info("Server gracefully stopped")
 	// Setup Server
 
 }
